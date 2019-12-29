817a1f5a221cbae66be173f1f5f0e9bb3fbec7c9
fx46 <31199346+fx46@users.noreply.github.com>
2019-12-29 06:14:50 -0500

    New vector struct

---
+++ FromZero/Vector.h
@@ -0,0 +1,55 @@
+#pragma once
+
+struct Vector
+{
+	float X, Y;
+	float &operator[](int Index) { return((&X)[Index]); }
+
+	Vector operator-()
+	{
+		Vector Result;
+		Result.X = -X;
+		Result.Y = -Y;
+
+		return Result;
+	}
+
+	Vector operator-(Vector V)
+	{
+		Vector Result;
+		Result.X = X - V.X;
+		Result.Y = Y - V.Y;
+
+		return Result;
+	}
+
+	Vector operator+(Vector V)
+	{
+		Vector Result;
+		Result.X = V.X + X;
+		Result.Y = V.Y + Y;
+
+		return Result;
+	}
+
+	Vector operator*(float F)
+	{
+		Vector Result;
+		Result.X = F * X;
+		Result.Y = F * Y;
+
+		return Result;
+	}
+
+	void operator*=(float F)
+	{
+		X *= F;
+		Y *= F;
+	}
+
+	void operator+=(Vector V)
+	{
+		X += V.X;
+		Y += V.Y;
+	}
+};
